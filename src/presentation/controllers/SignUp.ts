33a6ee65f3016c117ed1f58a8f83da6ffa4e3561
Danilo Martins <danilo.santos.martins2018@gmail.com>
2021-01-25 11:38:34 -0300

    add customized error fom missing param

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -5,11 +5,11 @@ import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/promise-function-async
   handle(httpRequest: HttpRequest): HttpResponse | any {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requireFields = ['name', 'email']
+    for (const field of requireFields) {
+      if (!httpRequest.body.email) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
