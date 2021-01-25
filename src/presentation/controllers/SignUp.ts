262ad5e1ba0e3239a2c46da12876a816045ac90e
Danilo Martins <danilo.santos.martins2018@gmail.com>
2021-01-25 17:37:12 -0300

    2. Criando erros personalizados

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,13 +1,13 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-erro'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 
-export class SignUpController {
-  // eslint-disable-next-line @typescript-eslint/promise-function-async
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse | any {
-    const requireFields = ['name', 'email']
+    const requireFields = ['name', 'email', 'password']
     for (const field of requireFields) {
-      if (!httpRequest.body.email) {
+      if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -32,4 +32,19 @@ describe('Signup Controller', () => {
     expect(httppResponse.statusCode).toBe(400)
     expect(httppResponse.body).toEqual(new MissingParamError('email'))
   })
+  test('Shoud return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Danilo',
+        email: 'danilo@gmail.com',
+        // password: 'facu12345',
+        passwordConfirmation: 'facu12345'
+      }
+    }
+
+    const httppResponse = sut.handle(httpRequest)
+    expect(httppResponse.statusCode).toBe(400)
+    expect(httppResponse.body).toEqual(new MissingParamError('password'))
+  })
 })
